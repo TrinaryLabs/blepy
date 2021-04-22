236d267a3447e34ceca5feaa9b99678b533e6eff
Fredrik Karlsson <f.v.carlsson@gmail.com>
2021-04-23 00:25:13 +0200

    feat: added 'service.py'

---
+++ src/service.py
@@ -0,0 +1,48 @@
+import dbus
+import dbus.service
+import bluez
+
+
+class Service(dbus.service.Object):
+
+    def __init__(self, bus, index, uuid, primary):
+        self.bus = bus
+        self.uuid = uuid
+        self.primary = primary
+        self.characteristics = []
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_properties(self):
+        return {
+            bluez.constants.GATT_SERVICE_IFACE: {
+                'UUID': self.uuid,
+                'Primary': self.primary,
+                'Characteristics': dbus.Array(
+                    self.get_characteristic_paths(),
+                    signature='o')
+            }
+        }
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_characteristic(self, characteristic):
+        self.characteristics.append(characteristic)
+
+    def get_characteristic_paths(self):
+        result = []
+        for chrc in self.characteristics:
+            result.append(chrc.get_path())
+        return result
+
+    def get_characteristics(self):
+        return self.characteristics
+
+    @dbus.service.method(bluez.constants.DBUS_PROP_IFACE,
+                         in_signature='s',
+                         out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != bluez.constants.GATT_SERVICE_IFACE:
+            raise bluez.exceptions.InvalidArgsException()
+
+        return self.get_properties()[bluez.constants.GATT_SERVICE_IFACE]
