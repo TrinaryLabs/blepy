1e864f358c515daa962ed9f65dad0c5be2e6a9da
Fredrik Karlsson <f.v.carlsson@gmail.com>
2021-04-23 00:26:09 +0200

    feat: added 'application.py'

---
+++ src/application.py
@@ -0,0 +1,14 @@
+import dbus
+
+
+class Application(dbus.service.Object):
+    def __init__(self, bus):
+        self.path = '/'
+        self.services = []
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_service(self, service):
+        self.services.append(service)
