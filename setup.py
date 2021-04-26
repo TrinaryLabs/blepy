89108abd9c160ef7ceb20650cf0cb4a000db509e
Fredrik Karlsson <f.v.carlsson@gmail.com>
2021-04-26 18:16:53 +0200

    Revert version: 0.2.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-required_packages = []
+required_packages = ['bluezero']
 extras_rel = ['bump2version', 'twine']
 extras_dev = extras_rel
 
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.0',
+    version='0.1.0',
 
     description='A class-based wrapper for the peripheral functionalities provided by the library python-bluezero',
     long_description=long_description,
